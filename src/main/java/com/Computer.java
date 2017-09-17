f7b5f693a31b22803126f4b3b8f48d398c7ea966
Manisha Awasthi <itsmanisha_15@yahoo.co.in>
2017-09-17 21:09:37 +0100

    refactoring

--- src/main/java/com/Computer.java
+++ src/main/java/com/Computer.java
@@ -3,7 +3,6 @@ package com;
 import java.util.Random;
 
 public class Computer extends GamePlayer implements RPSPlayer{
-//    private String gamePlayerName;
     Random index;
 
     static GameMove computerMove = null;
@@ -13,15 +12,6 @@ public class Computer extends GamePlayer implements RPSPlayer{
         super(name);
         index = new Random();
     }
-
-//    /**
-//     * Method to return Player's name
-//     *
-//     * @return gamePlayerName
-//     */
-//    public String getGamePlayerName() {
-//        return gamePlayerName;
-//    }
 
     /**
      * Randomly generates computer move based on defined moves in GameMove
